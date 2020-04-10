f61676d9dfa22e5a341d2baa5aac19dca9f41c6f
Óscar Ortiz <oscortiz@gmail.com>
2020-04-10 20:21:46 +0200

    Updates

---
+++ precache-manifest.c6f6e3dbe7dd728affa56b9671ef1139.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fff5caab415da83a29c3",
+    "url": "/fCC_Markdown_Previewer/static/css/main.410abe9f.chunk.css"
+  },
+  {
+    "revision": "fff5caab415da83a29c3",
+    "url": "/fCC_Markdown_Previewer/static/js/main.fff5caab.chunk.js"
+  },
+  {
+    "revision": "e727267d790bc1a15b9a",
+    "url": "/fCC_Markdown_Previewer/static/js/1.e727267d.chunk.js"
+  },
+  {
+    "revision": "358da9028ba11c5f5e40",
+    "url": "/fCC_Markdown_Previewer/static/js/runtime~main.358da902.js"
+  },
+  {
+    "revision": "8aff9e72be470eacdfe2d5080e55ff77",
+    "url": "/fCC_Markdown_Previewer/index.html"
+  }
+];
